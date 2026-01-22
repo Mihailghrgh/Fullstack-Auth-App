61ebdaa7f4f523d1063cafc76af92670f099a705
Gheorghe Mihai <mihailghrgh@gmail.com>
2026-01-22 18:16:45 +0200

    Bruohhhhhh

--- frontend/src/components/AuthPage/Register.tsx
+++ frontend/src/components/AuthPage/Register.tsx
@@ -1,13 +1,5 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+
 import { RegisterForm } from "../utils/registerForm";
 
 export default function Register() {
