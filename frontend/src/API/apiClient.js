ad17946a72a05ff9dcaaf4e3f26991b27eba68cf
Gheorghe Mihai <mihailghrgh@gmail.com>
2026-01-28 22:48:00 +0200

    t

--- frontend/src/API/apiClient.js
+++ frontend/src/API/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { redirect } from "next/navigation";
 
 const options = {
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: "http://localhost:5000",
 };
 const API = axios.create(options);
 
