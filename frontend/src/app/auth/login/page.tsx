6c2d9f222fed56764a927f7e69bad056836cc0b3
Gheorghe Mihai <mihailghrgh@gmail.com>
2026-01-28 21:30:09 +0200

    y

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,14 +1,14 @@
-"use server";
-import Login from "@/components/AuthPage/Login";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-async function LoginPage() {
-  const cookieStore = await cookies();
+import Login from "@/components/AuthPage/Login";
 
+export default async function LoginPage() {
+  const cookieStore = await cookies();
   const cookie = cookieStore.get("accessToken");
-  if (cookie) {
+
+  if (cookie?.value) {
     redirect("/auth/user");
   }
+
   return <Login />;
 }
-export default LoginPage;
